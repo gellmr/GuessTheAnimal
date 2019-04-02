df0ce45851c60418b5da8e14474ddf2854b5f68b
gellmr <gellmr@gmail.com>
2019-04-02 18:01:23 +0800

    Quiz logic working to select animal.

--- GuessTheAnimal/Controllers/GameController.cs
+++ GuessTheAnimal/Controllers/GameController.cs
@@ -9,6 +9,7 @@ namespace GuessTheAnimal.Controllers
 {
   public class GameController : Controller
   {
+    public List<Animal> Animals;
 
     public Dictionary<string, string> GetSessionAnswers()
     {
@@ -40,6 +41,7 @@ namespace GuessTheAnimal.Controllers
     public ActionResult Question2(string QuestionText, string Answer)
     {
       Dictionary<string, string> answers = GetSessionAnswers();
+      if (answers.ContainsKey(QuestionText)) { answers.Remove(QuestionText); } // overwrite previous answer is user navigates backward thru the quiz.
       answers.Add(QuestionText, Answer);
       SaveSessionAnswers(answers);
       QuestionViewModel model = new QuestionViewModel();
@@ -52,6 +54,7 @@ namespace GuessTheAnimal.Controllers
     public ActionResult Question3(string QuestionText, string Answer)
     {
       Dictionary<string, string> answers = GetSessionAnswers();
+      if (answers.ContainsKey(QuestionText)) { answers.Remove(QuestionText); } // overwrite previous answer is user navigates backward thru the quiz.
       answers.Add(QuestionText, Answer);
       SaveSessionAnswers(answers);
       QuestionViewModel model = new QuestionViewModel();
@@ -64,6 +67,7 @@ namespace GuessTheAnimal.Controllers
     public ActionResult Question4(string QuestionText, string Answer)
     {
       Dictionary<string, string> answers = GetSessionAnswers();
+      if (answers.ContainsKey(QuestionText)) { answers.Remove(QuestionText); } // overwrite previous answer is user navigates backward thru the quiz.
       answers.Add(QuestionText, Answer);
       SaveSessionAnswers(answers);
       QuestionViewModel model = new QuestionViewModel();
@@ -76,6 +80,7 @@ namespace GuessTheAnimal.Controllers
     public ActionResult Question5(string QuestionText, string Answer)
     {
       Dictionary<string, string> answers = GetSessionAnswers();
+      if (answers.ContainsKey(QuestionText)) { answers.Remove(QuestionText); } // overwrite previous answer is user navigates backward thru the quiz.
       answers.Add(QuestionText, Answer);
       SaveSessionAnswers(answers);
       QuestionViewModel model = new QuestionViewModel();
@@ -84,12 +89,41 @@ namespace GuessTheAnimal.Controllers
       return View("TextQuestion", model);
     }
 
+    // Convert "yes" into bool true and "no" into bool false.
+    private bool YesNo(string str)
+    {
+      if (str.ToLower().Equals("yes")) { return true; }
+      if (str.ToLower().Equals("true")) { return true; }
+
+      if (str.ToLower().Equals("no")) { return false; }
+      if (str.ToLower().Equals("false")) { return false; }
+
+      return false;
+    }
+
     [HttpPost]
     public ActionResult End(string QuestionText, string Answer)
     {
       Dictionary<string, string> answers = GetSessionAnswers();
+      if (answers.ContainsKey(QuestionText)){ answers.Remove(QuestionText); } // overwrite previous answer is user navigates backward thru the quiz.
       answers.Add(QuestionText, Answer);
       SaveSessionAnswers(answers);
+
+      InitAnimals();
+      IEnumerable<Animal> animalSet = Animals.Where(a =>
+        a.Legs == int.Parse( answers["How many legs does the animal have?"])
+        &&
+        a.Stripes == YesNo(answers["Does it have stripes?"])
+        &&
+        a.Swim == YesNo(answers["Does it swim?"])
+        &&
+        a.Color == answers["What color is the animal?"]
+        &&
+        a.Fly == YesNo(answers["Can it fly?"])
+      );
+
+      List<Animal> result = animalSet.ToList();
+
       QuestionViewModel model = new QuestionViewModel();
       return View("Guess", model);
     }
@@ -102,6 +136,33 @@ namespace GuessTheAnimal.Controllers
     public ActionResult Wrong()
     {
       return View();
+    }
+
+    private void InitAnimals()
+    {
+      // TODO: use localdb and seed data
+
+      Animals = new List<Animal>();
+      Animals.Add(new Animal {
+        Name = "Hippapotamus",
+        Fly = false,
+        Fast = false,
+        Legs = 4,
+        Stripes = false,
+        Swim = true,
+        Color = "grey"
+      });
+
+      Animals.Add(new Animal
+      {
+        Name = "Fish",
+        Fly = false,
+        Fast = true,
+        Legs = 0,
+        Stripes = false,
+        Swim = true,
+        Color = "grey"
+      });
     }
   }
 }
---
+++ GuessTheAnimal/Models/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+namespace GuessTheAnimal.Models
+{
+  public class Animal
+  {
+    public int Id;
+    public string Name;
+    public int Legs;
+    public bool Stripes;
+    public bool Swim;
+    public string Color;
+    public bool Fly;
+    public bool Fast;
+  }
+  
+}
