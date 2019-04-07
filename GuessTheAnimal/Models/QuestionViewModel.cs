2384d17b0fc465feb7bb9c6dc7eb08f031b636b4
gellmr <gellmr@gmail.com>
2019-04-07 15:26:54 +0800

    Added jquery validation on text input field.

--- GuessTheAnimal/Models/QuestionViewModel.cs
+++ GuessTheAnimal/Models/QuestionViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GuessTheAnimal.Models
 {
@@ -11,6 +12,8 @@ namespace GuessTheAnimal.Models
     public bool FirstQuestion = false;
     public string QuestionText = "";
     public string NextQuestion = "";
+
+    [Required]
     public string Answer = "";
   }
 }
